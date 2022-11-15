4869ae035897aad348b5c950be95a10afa0b3ea6
DanMarzo <marzogildan@gmail.com>
2022-11-15 15:08:54 -0300

    RegistrarPersonagem Metodo para listagem

--- RpgMvc/Controllers/PersonagensController.cs
+++ RpgMvc/Controllers/PersonagensController.cs
@@ -11,5 +11,44 @@ namespace RpgMvc.Controllers
         public string uriBase = "http://localhost:5270/Personagens/";
         //public string uriBase = "http://DanMarzo.somee.com/RpgApi/Personagens/";
         //public string uriBase = "https://bsite.net/luizfernando987/Personagens/";
+
+        [HttpGet]
+        public IActionResult IndexCreatePersonagem()
+        {
+            return View("CadastroPersonagem");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistrarPersonagemAsync(PersonagemViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "PostAdd";
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] = string.Format($"O personagem {u.Nome}, foi cadastrado com sucesso");
+                    return View("CadastroPersonagem");
+                }
+                else
+                {
+                    throw new Exception(serialized);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return View("CadastroPersonagem");
+            }
+        }
+
     }
 }
