04a1e0ec663b091725703c4c36d166d3ba57861f
DanMarzo <marzogildan@gmail.com>
2022-11-15 15:19:19 -0300

    PersonagemViewModel adicionado

---
+++ RpgMvc/Models/PersonagemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RpgMvc.Controllers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgMvc.Models
+{
+    public class PersonagemViewModel
+    {
+        public int        Id           { get; set; }
+        public string     Nome         { get; set; }
+        public int        PontosVida   { get; set; }
+        public int        Forca        { get; set; }
+        public int        Defesa       { get; set; }
+        public int        Inteligencia { get; set; }
+        public ClasseEnum Classe       { get; set; }
+    }
+}
