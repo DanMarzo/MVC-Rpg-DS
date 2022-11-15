c6b3b7e9572ed7079756554adbf309a442eea473
DanMarzo <marzogildan@gmail.com>
2022-11-15 15:10:49 -0300

    Instalação de pacotes

--- RpgMvc/Controllers/PersonagensController.cs
+++ RpgMvc/Controllers/PersonagensController.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace RpgMvc.Controllers
 {
