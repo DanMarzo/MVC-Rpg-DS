e11114d5c9824bf9f9a137953dafb126de3515ef
DanMarzo <marzogildan@gmail.com>
2022-11-15 15:16:42 -0300

    Classe Enum Criadas

---
+++ RpgMvc/Controllers/Enums/ClasseEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpgMvc.Controllers.Enums
+{
+    public enum ClasseEnum
+    {
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3
+    }
+}
