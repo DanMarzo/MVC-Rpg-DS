b2a44f5955955c55c83245404538383de2fa625a
DanMarzo <marzogildan@gmail.com>
2022-11-22 13:33:21 -0300

    Realizando manutenção em componentes

--- RpgMvc/Models/UsuarioViewModel.cs
+++ RpgMvc/Models/UsuarioViewModel.cs
@@ -8,7 +8,7 @@ namespace RpgMvc.Models
     public class UsuarioViewModel
     {
         public int    Id             { get; set; }
-        public string UserName       { get; set; }
+        public string Username       { get; set; }
         public string PasswordString { get; set; }
         public byte[] Foto           { get; set; }
     }
