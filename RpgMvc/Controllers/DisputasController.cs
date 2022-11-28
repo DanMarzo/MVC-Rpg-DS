8ca16688db61b2c359edf7f4ccff25b09dd828f8
DanMarzo <marzogildan@gmail.com>
2022-11-28 17:56:46 -0300

    DisputasController Criado realizara a ponte com a API

---
+++ RpgMvc/Controllers/DisputasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RpgMvc.Controllers
+{
+    public class DisputasController : Controller
+    {
+        public string uriBase = "http://localhost:5270/Disputas/";
+        //public string uriBase = "http://DanMarzo.somee.com/RpgApi/Disputas/";
+        //public string uriBase = "https://bsite.net/luizfernando987/Disputas/";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
